9c70d7088f36eea6829dd5d8af1e232d61e42231
Stephen Sreshta <stephensreshta@Stephens-Air.attlocal.net>
2019-12-04 15:00:06 -0500

    main.py update and html update

--- flaskapp/main.py
+++ flaskapp/main.py
@@ -4,23 +4,26 @@ from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
     return "Hello, World!"
 
-@app.route('/index')
+@app.route('/index', methods=["GET", "POST"])
 def index():
     url = request.args.get('youtube_url')
     return render_template("input.html", preset_url=url)
 
-@app.route('/output')
+@app.route('/output', methods=["GET", "POST"])
 def chord_output():
+    # return url
     url = request.args.get('youtube_url')
-    video_code = url[32:]
-
-    model_path = 'xgb_multifinal'
-    lr_predict.predict(url, model_path, f"{video_code}", 0.5, 24.5, 7, 1,
-                        'Models/', 'Results/', 3)
+    urlint = int(url)
+    return render_template("output.html", video=url)
+    # video_code = url[32:]
+    #
+    # model_path = 'xgb_multifinal'
+    # lr_predict.predict(url, model_path, f"{video_code}", 0.5, 24.5, 7, 1,
+    #                     'Models/', 'Results/', 3)
 
 if __name__ == "__main__":
     app.run(debug=True)
