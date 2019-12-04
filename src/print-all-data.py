d44942c2760fa449dd74faf9e5c202a32d4d7c84
Toby Manders <trm2124@gmail.com>
2019-12-04 17:16:45 -0500

    added htmlify and dummy functions

--- src/print-all-data.py
+++ src/print-all-data.py
@@ -1,11 +1,15 @@
 # Displays all data in the SQLite database
 
 import sqlite3
+import json
 
-conn = sqlite3.connect('../data/database.db')
-c = conn.cursor()
+def main():
+	conn = sqlite3.connect('../data/database.db')
+	c = conn.cursor()
 
-for row in c.execute('SELECT * FROM instock'):
-	print(row)
+	c.execute('SELECT * FROM instock')
+	data = c.fetchall()
+	return data
 
-conn.close()
+if __name__ == "__main__":
+	print(main())
